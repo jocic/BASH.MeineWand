53c9881c75cc869b07fa4cdb0e2247572dc53ce4
Đorđe Jocić <office@djordjejocic.com>
2019-01-15 18:59:49 +0100

    Polished & documented script for creating onion-like environment.

--- readme.md
+++ readme.md
@@ -6,11 +6,9 @@ Collection of personal scripts for Firewall management.
 
 ## Notes
 
-**Note #1:** Some of these scripts may only work with my hardware, and some may look seemingly random. Once I upload my entire collection, I will start polishing & documenting them.
+**Note #1:** As I use TunnelBear (it was generally proven to be the best VPN available) you may notice that IP addresses of their servers are used in some scripts ex. the one for creating an Onion-like environment. I'll try to change that in the future and support other VPN providers ex. HMA, NordVPN, etc. BEAR with me for now.
 
-**Note #2:** Also, as I use TunnelBear (it was generally proven to be the best VPN available) you may notice that IP addresses of their servers are used in some scripts ex. the one for creating an Onion-like environment. I'll try to change that in the future and support other VPN providers ex. HMA, NordVPN, etc. BEAR with me for now.
-
-**Note #3:** All scripts are POSIX-compliant.
+**Note #2:** All scripts are POSIX-compliant.
 
 ## Usage
 
@@ -18,3 +16,4 @@ Collection of personal scripts for Firewall management.
 *   [init-advanced.sh](src/init-advanced.sh) Advanced initialization script will do everything as the basic one. In addition it will allow HTTP, HTTPS & Mail-related outbound traffic.
 *   [check.sh](src/check.sh) Script is used for checking UFW status every second in a minute. If the UFW is inactive, all network interfaces are immediately shut down to prevent a potential breach. It should be ran by a CRON job.
 *   [sandbox.sh](src/sandbox.sh) Sandboxing script allows you to create an environment in which only traffic toward specific online services is allowed - Twitter, GitHub, LinkedIn & GoDaddy (UK). All inbound and outbound traffic that isn't related to the mentioned services is blocked, even DNS-related traffic.
+*   [onion.sh](src/onion.sh) This script is used for creating an onion-like environment by utilizing 2 or more VPN servers of your desired VPN provider. Number of physical or virtual machines depends on the number layers you selected.
--- src/onion.sh
+++ src/onion.sh
@@ -29,46 +29,86 @@
 # OTHER DEALINGS IN THE SOFTWARE.                                 #
 ###################################################################
 
-if [ "$1" = "1" ]; then # Layer 1
-    
-    echo "y" | ufw reset;
-    
-    ufw enable;
-    
-    ufw default deny incoming;
-    ufw default deny outgoing;
-    
-    ufw allow out on enp0s3 to any port 68 proto udp comment "Regular DHCP";
-    
-    ufw allow out on enx0c5b8f279a64 to 109.123.74.101 proto udp comment "TunnelBear (UK)";
-    
-    ufw allow out on tun0 to 8.8.8.8 port 53 proto udp comment "Google DNS #1";
-    ufw allow out on tun0 to 8.8.4.4 port 53 proto udp comment "Google DNS #2";
-    
-    ufw allow out on tun0 to any port 443 proto tcp comment "HTTPS";
-    
-    ufw allow out on tun0 to any port 465 proto tcp comment "SMTP (SSL)"
-    ufw allow out on tun0 to any port 995 proto tcp comment "POP3 (SSL)"
-    
-elif [ "$1" = "2" ]; then # Layer 2
-    
-    echo "y" | ufw reset;
-    
-    ufw enable;
-    
-    ufw default deny incoming;
-    ufw default deny outgoing;
-    
-    ufw allow out on enp0s3 to any port 68 proto udp comment "Regular DHCP";
-    
-    ufw allow out on enp0s3 to 185.246.130.5 proto udp comment "TunnelBear (SE)";
-    
-    ufw allow out on tun0 to 8.8.8.8 port 53 proto udp comment "Google DNS #1";
-    ufw allow out on tun0 to 8.8.4.4 port 53 proto udp comment "Google DNS #2";
-    
-    ufw allow out on tun0 to any port 443 proto tcp comment "HTTPS";
-    
-    ufw allow out on tun0 to any port 465 proto tcp comment "SMTP (SSL)"
-    ufw allow out on tun0 to any port 995 proto tcp comment "POP3 (SSL)"
-    
+##################
+# Core Variables #
+##################
+
+user_id="$(id -u)";
+network_interface="$1";
+vpn_interface="$2";
+vpn_ip_address="$3";
+
+#############################
+# Step 1 - Check Privileges #
+#############################
+
+if [ "$user_id" != "0" ]; then
+    printf "Error: You need to run this script with root privileges.\n" && exit;
 fi
+
+#############################
+# Step 2 - Check Parameters #
+#############################
+
+if [ -z "$vpn_ip_address" ]; then
+    printf "Error: You need to enter an IP address of a VPN server.\n" && exit;
+fi
+
+###############################
+# Step 3 - Process Parameters #
+###############################
+
+if [ -z "$network_interface" ]; then
+    network_interface="all";
+fi
+
+if [ -z "$vpn_interface" ]; then
+    vpn_interface="tun0";
+fi
+
+###########################
+# Step 4 - Reset Firewall #
+###########################
+
+printf "[+] Reseting firewall..\n";
+
+echo "y" | /usr/sbin/ufw reset > /dev/null 2>&1;
+
+############################
+# Step 5 - Enable Firewall #
+############################
+
+printf "[+] Enabling firewall..\n";
+
+ufw enable > /dev/null 2>&1;
+
+###########################################
+# Step 6 - Disable All Traffic By Default #
+###########################################
+
+printf "[+] Disabling all inbound & outbound traffic...\n";
+
+ufw default deny incoming > /dev/null 2>&1;
+ufw default deny outgoing > /dev/null 2>&1;
+
+##############################
+# Step 7 - Define Core Rules #
+##############################
+
+printf "[+] Defining core rules...\n";
+
+ufw allow out on any to any port 68 proto udp comment "Regular DHCP" > /dev/null 2>&1;
+
+ufw allow out on "$network_interface" to "$vpn_ip_address" proto udp comment "VPN's IP" > /dev/null 2>&1;
+
+ufw allow out on "$vpn_interface" to 8.8.8.8 port 53 proto udp comment "Google DNS #1" > /dev/null 2>&1;
+ufw allow out on "$vpn_interface" to 8.8.4.4 port 53 proto udp comment "Google DNS #2" > /dev/null 2>&1;
+
+ufw allow out on "$vpn_interface" to any port 80 proto tcp comment "HTTP (TCP)" > /dev/null 2>&1;
+ufw allow out on "$vpn_interface" to any port 80 proto udp comment "HTTP (UDP)" > /dev/null 2>&1;
+
+ufw allow out on "$vpn_interface" to any port 443 proto tcp comment "HTTPS (TCP)" > /dev/null 2>&1;
+ufw allow out on "$vpn_interface" to any port 443 proto udp comment "HTTPS (UDP)" > /dev/null 2>&1;
+
+ufw allow out on "$vpn_interface" to any port 465 proto tcp comment "SMTP (SSL)" > /dev/null 2>&1;
+ufw allow out on "$vpn_interface" to any port 995 proto tcp comment "POP3 (SSL)" > /dev/null 2>&1;
